f1d08e35a4f5a494cfe2490ad4ba360c6fc34d75
Gabriel Teles <bilebile2020@gmail.com>
2026-03-18 16:35:46 -0300

    Auditação de resposta

--- src/services/fetchNotes.ts
+++ src/services/fetchNotes.ts
@@ -3,15 +3,18 @@ import type { Note } from "../types/Notes";
 async function fetchNotes(): Promise<Note[]> {
     const res = await fetch(`${import.meta.env.VITE_API_URL}/notes`, { credentials: "include" });
     const body = await res.json();
+    console.log(body);
 
     if (body.error === "InvalidToken") {
         const refreshRes = await fetch(`${import.meta.env.VITE_API_URL}/auth/refresh`, { credentials: "include" });
         const refreshBody = await refreshRes.json();
+        console.log(refreshBody);
         if (refreshBody.error) return refreshBody.error;
 
         if (refreshBody.message === "TokenCreated") {
             const retryRes = await fetch(`${import.meta.env.VITE_API_URL}/notes`, { credentials: "include" });
             const retryBody = await retryRes.json();
+            console.log(retryBody);
             if (retryBody.error) return retryBody.error;
             return retryBody as Note[];
         }
