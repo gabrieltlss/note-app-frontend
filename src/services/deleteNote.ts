576eb3a1bf8c295d476e9630a17d74d9b8b3c2fa
Gabriel Teles <bilebile2020@gmail.com>
2026-03-18 17:09:11 -0300

    Adiciona logs

--- src/services/deleteNote.ts
+++ src/services/deleteNote.ts
@@ -4,10 +4,12 @@ async function deleteNoteById(noteId: string) {
         { credentials: "include", method: "DELETE" }
     );
     const body = await res.json();
+    console.log(body);
 
     if (body.error === "InvalidToken") {
         const refreshRes = await fetch(`${import.meta.env.VITE_API_URL}/auth/refresh`, { credentials: "include" });
         const refreshBody = await refreshRes.json();
+        console.log(refreshBody);
         if (refreshBody.error) return refreshBody.error;
 
         if (refreshBody.message === "TokenCreated") {
@@ -16,6 +18,7 @@ async function deleteNoteById(noteId: string) {
                 { credentials: "include", method: "DELETE" }
             );
             const retryBody = await retryRes.json();
+            console.log(retryBody);
             if (retryBody.error) return retryBody.error;
             return retryBody.message;
         }
