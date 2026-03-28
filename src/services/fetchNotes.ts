69e1d01731a5f3b7d842fce8da1c6829b119ae75
Gabriel Teles <bilebile2020@gmail.com>
2026-03-28 15:41:33 -0300

    Ajuste dos conteúdos das notas

--- src/services/fetchNotes.ts
+++ src/services/fetchNotes.ts
@@ -3,7 +3,6 @@ import type { Note } from "../types/Notes";
 async function fetchNotes(): Promise<Note[]> {
     const res = await fetch(`${import.meta.env.VITE_API_URL}/notes`, { credentials: "include" });
     const body = await res.json();
-    console.log(body);
 
     if (body.error === "InvalidToken") {
         const refreshRes = await fetch(`${import.meta.env.VITE_API_URL}/auth/refresh`, { credentials: "include" });
